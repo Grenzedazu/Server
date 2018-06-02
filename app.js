da1f5d2c9dd830f9a1075e221cf7d0b1d0f04474
Levin <UPSIDEJin@outlook.com>
2018-06-02 16:07:05 +0800

    v0.0.2 prot

--- app.js
+++ app.js
@@ -16,4 +16,6 @@ const router = require('./routes')
 app.use(router.routes())
 
 // 启动程序，监听端口
-app.listen(config.port, () => debug(`listening on port ${config.port}`))
+app.listen(config.port, function(){
+    console.log("success")
+})
